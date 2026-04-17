e8da08bd86b447bf23b25383a4ec6374b8dea519
sujitpatel2739 <20251987sujitpatel2739@users.noreply.github.com>
2026-04-17 22:11:24 +0530

    fixed all reaper_front files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -9,26 +8,4 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
->>>>>>> 3b424f8 (feat: sync package files for cloudflare)
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
->>>>>>> dcaed53 (feat: sync package files for cloudflare)
-);
+)
